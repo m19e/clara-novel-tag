ffb0e9b43eb08c63bd7c1b8cdf45e296d61b8df2
m19e <nexus.me7290@gmail.com>
2021-02-10 14:11:32 +0900

    Try tag autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, createRef } from "react";
+import Tags, { Tag } from "react-tag-autocomplete";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tags, setTags] = useState([
+        { id: 1, name: "Apples" },
+        { id: 2, name: "Pears" },
+    ]);
+    const [suggests, setSuggests] = useState([
+        { id: 3, name: "Bananas" },
+        { id: 4, name: "Mangos" },
+        { id: 5, name: "Lemons" },
+        { id: 6, name: "Apricots" },
+    ]);
+    const tagsRef = createRef<Tags>();
+
+    const handleAddTag = (tag: Tag) => {
+        setTags([...tags, tag as { id: number; name: string }]);
+    };
+
+    const handleDeleteTag = (i: number) => {
+        const t = tags.slice(0);
+        t.splice(i, 1);
+        setTags(t);
+    };
+
+    return <Tags ref={tagsRef} tags={tags} suggestions={suggests} onDelete={handleDeleteTag} onAddition={handleAddTag} />;
 }
 
 export default App;
